17e7fb8b6e52a25180b5a55cdf75f067f642b398
xavier <omondix214@gmail.com>
2023-04-26 00:40:27 +0300

    url

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.get("/stk", (req, res) => {
 });
 
 app.post("/stk", generateToken, async (req, res) => {
+  const callbackurl = process.env.CALLBACK_URL;
+  console.log(callbackurl);
   const phone = req.body.phone.substring(1);
   const amount = req.body.amount;
 
@@ -78,7 +80,7 @@ app.post("/stk", generateToken, async (req, res) => {
         PartyA: `254${phone}`,
         PartyB: shortcode,
         PhoneNumber: `254${phone}`,
-        CallBackURL: `/callback`,
+        CallBackURL: `${callbackurl}/callback`,
         AccountReference: `254${phone}`,
         TransactionDesc: "Test",
       },
