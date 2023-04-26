f1b02fa93275cae91a85655ffdc7baed3c0319e5
xavier <omondix214@gmail.com>
2023-04-26 10:47:29 +0300

    variables

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/stk", generateToken, async (req, res) => {
         PartyA: `254${phone}`,
         PartyB: shortcode,
         PhoneNumber: `254${phone}`,
-        CallBackURL: `${callbackurl}/callnack`,
+        CallBackURL: `${callbackurl}/callback`,
         AccountReference: `254${phone}`,
         TransactionDesc: "Test",
       },
