efcc338b1b557f584a25e7400152ca3efe24bca0
xavier <omondix214@gmail.com>
2023-05-30 11:24:35 +0300

    removed callback url

--- index.js
+++ index.js
@@ -105,10 +105,4 @@ app.post("/stk", generateToken, async (req, res) => {
   }
 });
 
-//Create a route to handle the callback
-app.post("/callback", (req, res) => {
-  console.log("Received callback:", req.body);
-  res.send("Callback received");
-});
-
 app.listen(process.env.PORT, () => console.log("Server running on port 8080"));
